56fd79994bdea81fdea74705892496d89e469d66
marcelog5 <marcelovictorg2@gmail.com>
2022-04-14 18:49:16 -0300

    Added image colored
    Co-authored-by: Washington Bispo <juniorbispo515@gmail.com>

--- src/Pages/Coordenadas/index.js
+++ src/Pages/Coordenadas/index.js
@@ -4,6 +4,7 @@ import { Background } from "./styles";
 
 const Coordenadas = () => {
   const [coordenada, setCoordenada] = React.useState([]);
+  const [realCoordenada, setRealCoordenada] = React.useState([]);
   const [coordenadas, setCoordenadas] = React.useState([]);
   const [isSecondClick, setIsSecondClick] = React.useState(false);
 
@@ -17,6 +18,47 @@ const Coordenadas = () => {
     } else {
       return [oElement.x, oElement.y];
     }
+  };
+
+  const placeImage = (x1, y1, x2, y2) => {
+    const e = document.createElement("div");
+    e.style.position = "absolute";
+
+    const red = Math.random() * 255;
+    const green = Math.random() * 255;
+    const blue = Math.random() * 255;
+
+    e.style.backgroundColor = `rgba(${red},${green},${blue},0.7)`;
+    e.style.border = `1px solid rgba(${red},${green},${blue},0.7)`;
+
+    let auxmin = 0;
+    let auxmax = 0;
+
+    auxmin = Math.min(x1, x2);
+    auxmax = Math.max(x1, x2);
+
+    x1 = auxmin;
+    x2 = auxmax;
+
+    auxmin = Math.min(y1, y2);
+    auxmax = Math.max(y1, y2);
+
+    y1 = auxmin;
+    y2 = auxmax;
+
+    var w = x2 - x1;
+    var h = y2 - y1;
+
+    e.style.left = x1 + "px";
+    e.style.top = y1 + "px";
+
+    console.log(w);
+    e.style.width = w + "px";
+
+    console.log(h);
+    e.style.height = h + "px";
+
+    document.body.appendChild(e);
   };
 
   const handleClickImage = (e) => {
@@ -46,8 +88,10 @@ const Coordenadas = () => {
 
     if (!isSecondClick) {
       setCoordenada([PosX, PosY]);
+      setRealCoordenada([e.pageX, e.pageY]);
       setIsSecondClick(true);
     } else {
+      placeImage(realCoordenada[0], realCoordenada[1], e.pageX, e.pageY);
       setCoordenadas([
         ...coordenadas,
         [coordenada[0], coordenada[1], PosX, PosY],
